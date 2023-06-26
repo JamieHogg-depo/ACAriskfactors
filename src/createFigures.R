bb68f1db353ff3bfe76ba542fbbaf0a7e44caa32
Jamie Hogg <jamie-hogg@hotmail.com>
2023-06-27 09:33:24 +1000

    Updated create figures

--- src/createFigures.R
+++ src/createFigures.R
@@ -2,171 +2,178 @@
 ## FIGURES ## ------------------------------------------------------------------
 ## -----------------------------------------------------------------------------
 
-# SA4 level - HT vs modeled ####
-b_est$summ_mu_sa4 %>% 
-  ggplot(aes(y = median, ymin = lower, ymax = upper,
-             x = HT, xmin = HT_lower, xmax = HT_upper))+
-  theme_bw()+geom_abline(col="red")+
-  geom_errorbar(col = "grey")+geom_errorbarh(col = "grey")+
-  geom_point()+
-  facet_grid(.~model)+
-  labs(y = "Modelled prevalence estimate",
-       x = "Direct prevalence estimate")
-if(export) jsave("sa4_directvsmodeled.png", square = F)
+source("src/ms.R")
 
-# SA4 level - ARB, RRMSE #### 
-(b_est$summ_mu_sa4 %>% 
-   dplyr::select(ps_sa4, n, RRMSE, model) %>% 
-   pivot_wider(names_from = model, values_from = RRMSE) %>% 
-   arrange(TSLN) %>% mutate(x = 1:nrow(.)) %>% dplyr::select(-ps_sa4) %>% 
-   pivot_longer(-c(x, n)) %>% 
-   ggplot(aes(y = 10*value, x = x, color = name))+
-   theme_bw()+
-   geom_path()+geom_point(aes(size = n))+
-   scale_color_manual(values = jcol$color,
-                      breaks = jcol$model)+
-   labs(y = "RRMSE (x10)", x = "", col = "",
-        size = "Sample size")+
-   theme(legend.position = "bottom",
-         legend.box = "vertical")+
-   scale_size_binned(n.breaks = 5))+
-  (b_est$summ_mu_sa4 %>% 
-     dplyr::select(ps_sa4, n, ARB, model) %>% 
-     pivot_wider(names_from = model, values_from = ARB) %>% 
-     arrange(TSLN) %>% mutate(x = 1:nrow(.)) %>% dplyr::select(-ps_sa4) %>% 
-     pivot_longer(-c(x, n)) %>% 
-     ggplot(aes(y = 10*value, x = x, color = name))+
-     theme_bw()+
-     geom_path()+geom_point(aes(size = n))+
-     scale_color_manual(values = jcol$color,
-                        breaks = jcol$model)+
-     labs(y = "ARB (x10)", x = "", col = "",
-          size = "Sample size")+
-     theme(legend.position = "bottom",
-           legend.box = "vertical")+
-     scale_size_binned(n.breaks = 5))
-if(export) jsave("sa4_rrmse_arb.png", square = F)
+lookup <- data.frame(rf = names(raw_est),
+                     sha = c("exercise", "exercise", "alcohol", 
+                             "fruit", "obese", "overweight",
+                             "smoking", "overweight"))
 
-# What about relative measures??
+## START FOR LOOP #### ---------------------------------------------------------
+for(k in 1:8){
+  
+  rf <- names(raw_est)[k]
+  sha_var = paste0("SHAPHA_estimate_", lookup[lookup$rf == rf,]$sha)
+  message("Started ", k, ": ", rf)
+  
+  # load data
+  modelled_est <- readRDS(file = paste0("data/summary_files/", rf, "_b1.rds"))
+  
+## Scatter: SHA PHA vs ACA #### ------------------------------------------------
+  
+modelled_est$summ$sa2 %>% 
+    ggplot(aes(x = mu_median, xmin = mu_lower, xmax = mu_upper, 
+               y = .data[[sha_var]]))+
+    theme_bw()+
+    geom_errorbar(col = "grey")+
+    geom_abline()+
+    geom_point()+
+    labs(y = "Point estimtes from SHAA (PHA level)",
+         x = "Current posterior medians (SA2 level)")+
+    ylim(0,1)+xlim(0,1)
+  
+# save object
+jsave(filename = paste0("scatter_shaphavsaca_", rf ,".png"), 
+      base_folder = paste0(base_folder, "/figures"),
+      square = F)
 
-# Violin plots ####
-b_est$summ_mu %>% 
-  ggplot(aes(y = model, fill = model, x = median))+
-  theme_bw()+
-  geom_violin()+
-  labs(y = "",
-       x = "Modeled prevalence estimate")+
-  theme(legend.position = "none")+
-  scale_fill_manual(values = jcol$color,
-                    breaks = jcol$model)
-if(export) jsave("violin.png", square = F)
+## Boxplot: ACA vs SEIFA and Remoteness #### -----------------------------------
 
-# Boxplot of posterior medians - IRSD ####
-prev_median_wide %>%
-  left_join(.,dplyr::select(aux2, ps_area, ABS_irsd_decile_nation, ra_sa2_3c),
-            by = "ps_area") %>%
-  pivot_longer(-c(ps_area, ABS_irsd_decile_nation, ra_sa2_3c)) %>%
-  # rename IRSD categories
-  mutate(ABS_irsd_decile_nation = factor(ABS_irsd_decile_nation, levels = 1:10,
+seifa <- modelled_est$summ$sa2 %>% 
+  mutate(ABS_irsd_decile_nation_complete = factor(ABS_irsd_decile_nation_complete, 
+                                                  levels = 1:10,
                                          labels = c("Least advantaged", 
                                                     as.character(2:9), 
                                                     "Most advantaged"))) %>% 
-  ggplot(aes(x = value, fill = name, y = ABS_irsd_decile_nation))+
+  ggplot(aes(x = mu_median,
+             y = ABS_irsd_decile_nation_complete))+
   theme_bw()+
   geom_boxplot()+
-  labs(x = "Modeled prevalence estimate",
-       y = "IRSD deciles",
-       fill = "")+
-  theme(legend.position = "bottom")+
-  scale_fill_manual(values = jcol$color,
-                    breaks = jcol$model)
-if(export) jsave("boxplot_byseifa.png", square = F)
+  labs(y = "",
+       x = "")
 
-# Boxplot of posterior medians - Remoteness ####
-prev_median_wide %>%
-  left_join(.,dplyr::select(aux2, ps_area, ABS_irsd_decile_nation, ra_sa2_3c),
-            by = "ps_area") %>%
-  pivot_longer(-c(ps_area, ABS_irsd_decile_nation, ra_sa2_3c)) %>%
-  ggplot(aes(x = value, fill = name, y = ra_sa2_3c))+
+ra <- modelled_est$summ$sa2 %>% 
+  mutate(ra_sa2 = factor(ra_sa2, c("Major Cities", "Inner Regional", 
+                                   "Outer Regional", "Remote", 
+                                   "Very Remote"))) %>% 
+  ggplot(aes(x = mu_median,
+             y = ra_sa2))+
   theme_bw()+
   geom_boxplot()+
-  #facet_grid(.~ra_sa2_3c, labeller = label_wrap_gen(multi_line = TRUE))+
-  labs(x = "Modeled prevalence estimate",
-       y = "",
-       fill = "")+
-  theme(legend.position = "bottom")+
-  scale_fill_manual(values = jcol$color,
-                    breaks = jcol$model)
-if(export) jsave("boxplot_byremoteness.png", square = F)
+  labs(y = "",
+       x = "Posterior medians")+
+  scale_y_discrete(limits=rev)
 
-# Compare estimates from models ####
-dplyr::select(b_est$summ_mu, median, lower, upper, model, ps_area) %>% 
-  pivot_wider(names_from = model, values_from = c(median, lower, upper)) %>% 
-  ggplot(aes(y = median_TSLN, ymin = lower_TSLN, ymax = upper_TSLN,
-             x = median_ELN, xmin = lower_ELN, xmax = upper_ELN))+
-  theme_bw(base_size = 20)+
-  geom_abline(col = "red")+
-  geom_hline(yintercept = HT_mu)+
-  geom_vline(xintercept = HT_mu)+
-  geom_errorbar(col="grey")+
-  geom_errorbarh(col="grey")+
-  geom_point()+
-  xlim(0,1)+ylim(0,1)+
-  labs(y = "TSLN model",
-       x = "ELN model")
-if(export) jsave("scatter_TSLNvsELN.png", square = T)
+# Create layout
+lay <- rbind(c(1),
+             c(2))
+full_plt <- grid.arrange(grobs = list(seifa, ra), 
+                               layout_matrix  = lay)
 
-dplyr::select(b_est$summ_mu, median, lower, upper, model, ps_area) %>% 
-  pivot_wider(names_from = model, values_from = c(median, lower, upper)) %>% 
-  ggplot(aes(y = median_TSLN, ymin = lower_TSLN, ymax = upper_TSLN,
-             x = median_LOG, xmin = lower_LOG, xmax = upper_LOG))+
-  theme_bw(base_size = 20)+
-  geom_abline(col = "red")+
-  geom_hline(yintercept = HT_mu)+
-  geom_vline(xintercept = HT_mu)+
-  geom_errorbar(col="grey")+
-  geom_errorbarh(col="grey")+
-  geom_point()+
-  xlim(0,1)+ylim(0,1)+
-  labs(y = "TSLN model",
-       x = "LOG model")
-if(export) jsave("scatter_TSLNvsLOG.png", square = T)
+# save object
+jsave(filename = paste0("boxplot_avaseifaremoteness_", rf ,".png"), 
+      base_folder = paste0(base_folder, "/figures"),
+      plot = full_plt,
+      square = F)
 
-dplyr::select(b_est$summ_mu, median, lower, upper, model, ps_area) %>% 
-  pivot_wider(names_from = model, values_from = c(median, lower, upper)) %>% 
-  ggplot(aes(y = median_ELN, ymin = lower_ELN, ymax = upper_ELN,
-             x = median_LOG, xmin = lower_LOG, xmax = upper_LOG))+
-  theme_bw(base_size = 20)+
-  geom_abline(col = "red")+
-  geom_hline(yintercept = HT_mu)+
-  geom_vline(xintercept = HT_mu)+
-  geom_errorbar(col="grey")+
-  geom_errorbarh(col="grey")+
-  geom_point()+
-  xlim(0,1)+ylim(0,1)+
-  labs(y = "ELN model",
-       x = "LOG model")
-if(export) jsave("scatter_ELNvsLOG.png", square = T)
+# cleanup
+rm(seifa, ra)
 
-# Caterpillar plots - by model - with CI  #### ---------------------------------
+## Caterpillar plots #### ------------------------------------------------------
 
-# Prevalence
-b_est$summ_mu %>% 
-  group_by(model) %>% 
-  mutate(rank = rank(median, ties.method = "first")) %>%
-  ggplot(aes(y = median, ymin = lower, ymax = upper,
+modelled_est$summ$sa2 %>% 
+  mutate(rank = rank(mu_median, ties.method = "first")) %>%
+  ggplot(aes(y = mu_median, ymin = mu_lower, ymax = mu_upper,
              x = rank, 
-             col = median))+theme_bw()+
+             col = mu_median))+
+  theme_bw()+
   geom_errorbar(col = "grey")+
+  geom_hline(yintercept = raw_est[[k]]$national[1])+
   geom_point()+
-  facet_wrap(.~model)+
   scale_color_viridis_c(begin = 0.3, end = 1, 
-                        direction = 1,
-                        option = "B")+
-  geom_hline(yintercept = HT_mu)+
+                      direction = 1,
+                      option = "B")+
   labs(y = "Modeled prevalence estimates",
        x = "")+
   theme(legend.position = "none")
-if(export) jsave("cat_prev_medianci.png", square = F)
 
-## END SCRIPT ## --------------------------------------------------------------
+# save object
+jsave(filename = paste0("catterpillar_", rf ,".png"), 
+      base_folder = paste0(base_folder, "/figures"),
+      square = F)
+
+## Scatter - SA4 direct vs SA4 modelled #### -----------------------------------
+
+# setup
+temp <- left_join(modelled_est$summ$sa4, raw_est[[k]]$sa4, by = "ps_sa4")
+for_range <- with(temp, c(lower, upper, HT - 1.96 * HT_SE, HT + 1.96 * HT_SE))
+st_range <- range(for_range)
+  
+# plot
+  temp %>% 
+  ggplot(aes(y = median, ymin = lower, ymax = upper,
+             x = HT, 
+             xmin = HT - 1.96 * HT_SE, 
+             xmax = HT + 1.96 * HT_SE,
+             col = log(HT_CV)))+
+  theme_bw()+
+  geom_errorbar(col = "grey")+
+  geom_errorbarh(col = "grey")+
+  geom_abline()+
+  geom_point()+
+  scale_color_viridis_c(begin = 0, end = 1, 
+                        direction = -1,
+                        option = "A")+
+  labs(col = "Log\ndirect\nestimate\nCV",
+       y = "Modelled",
+       x = "Direct")+
+  ylim(st_range[1],st_range[2])+xlim(st_range[1],st_range[2])
+
+# save object
+jsave(filename = paste0("scatter_sa4directvsmodelled_", rf ,".png"), 
+      base_folder = paste0(base_folder, "/figures"),
+      square = F)
+
+# cleanup
+rm(temp, for_range, st_range)
+
+## Scatter - MSB direct vs modelled #### ---------------------------------------
+
+# setup
+temp <- left_join(modelled_est$summ$msb, raw_est[[k]]$msb, by = "ps_majorstatebench")
+for_range <- with(temp, c(mu_lower, mu_upper, HT - 1.96 * HT_SE, HT + 1.96 * HT_SE))
+st_range <- range(for_range)
+
+# plot
+temp %>% 
+  ggplot(aes(y = mu_median, ymin = mu_lower, ymax = mu_upper,
+             x = HT, 
+             xmin = HT - 1.96 * HT_SE, 
+             xmax = HT + 1.96 * HT_SE,
+             col = log(HT_CV)))+
+  theme_bw()+
+  geom_errorbar(col = "grey")+
+  geom_errorbarh(col = "grey")+
+  geom_abline()+
+  geom_point()+
+  scale_color_viridis_c(begin = 0, end = 1, 
+                        direction = -1,
+                        option = "A")+
+  labs(col = "Log\ndirect\nestimate\nCV",
+       y = "Modelled",
+       x = "Direct")+
+  ylim(st_range[1],st_range[2])+xlim(st_range[1],st_range[2])
+
+# save object
+jsave(filename = paste0("scatter_msbdirectvsmodelled_", rf ,".png"), 
+      base_folder = paste0(base_folder, "/figures"),
+      square = F)
+
+# cleanup
+rm(temp, for_range, st_range)
+
+## FINISH FOR LOOP #### --------------------------------------------------------
+
+}
+
+## END SCRIPT #### -------------------------------------------------------------
