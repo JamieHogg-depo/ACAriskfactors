d50db9d47bd54189d39cff16d3ec6899c6192c07
Jamie Hogg <jamie-hogg@hotmail.com>
2023-07-26 10:26:39 +1000

    Added ModelledEstimates as a csv

---
+++ src/wrangle/createACADataForJournal.R
@@ -0,0 +1,30 @@
+##
+# Create data for Journal page
+##
+
+source('src/ms.R')
+
+# Packages
+library(readr)
+library(readxl)
+library(openxlsx)
+
+# create excel
+summsa2all %>% 
+  dplyr::select(SA2, model, contains(c("mu_", "rr_"))) %>% 
+  dplyr::select(-contains("logrr_")) %>% 
+  rename(risk_factor = model,
+         SA2_2016 = SA2) %>% 
+  mutate(risk_factor = case_when(
+    risk_factor == "activityleis" ~ "Inadequate physical activity (leisure)",
+    risk_factor == "activityleiswkpl" ~ "Inadequate physical activity (all)",
+    risk_factor == "alcohol" ~ "Risky alcohol consumption",
+    risk_factor == "diet" ~ "Inadequate diet",
+    risk_factor == "obesity" ~ "Obese",
+    risk_factor == "overweight" ~ "Overweight/obese",
+    risk_factor == "smoking" ~ "Current smoking",
+    risk_factor == "waist_circum" ~ "Risky waist circumference"
+  )) %>% 
+  write.csv(., "ModelledEstimates.csv")
+
+## END SCRIPT #### -------------------------------------------------------------
