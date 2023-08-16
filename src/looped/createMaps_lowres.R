b442169b985789d65b3cd5cc77bfa32305b94d5b
Jamie Hogg <jamie-hogg@hotmail.com>
2023-08-16 11:58:37 +1000

    Added joint figures as separate figures

--- src/looped/createMaps_lowres.R
+++ src/looped/createMaps_lowres.R
@@ -91,6 +91,162 @@ rm(base, base_boxes, llegend, base_legend, lay, full_inset_plt,
    modelled_est, pha_map, rar, pha_estimates)
 message("---- Finished SHAA obese")
 
+## SHAA estimates - obesity - joined ## ----------------------------------------
+
+## PHA ##
+
+# Load objects
+modelled_est <- readRDS("data/summary_files/obesity_b1.rds")
+pha_map <- suppressMessages(modelled_est$summ$sa2_map %>% 
+                              group_by(pha) %>% 
+                              summarise(geometry = st_union(geometry)))
+
+# some new datasets and objects
+pha_estimates <- SHA_pha %>% 
+  filter(!is.na(shaout_smoking_estimate)) %>% 
+  right_join(., pha_map) %>% 
+  mutate(median = shaout_obese_estimate) %>% 
+  st_as_sf()
+
+# squish the top and lower 2.5 quantiles
+rar<- unname(quantile(modelled_est$summ$sa2$mu_median, p = c(0.01,0.99), na.rm = T))
+
+# base map
+base <-pha_estimates %>% 
+  ggplot()+
+  theme_void()+
+  geom_sf(aes(fill = median), col = NA)+
+  scale_fill_viridis_c(begin = 0, end = 1, 
+                       direction = -1,
+                       option = "F", 
+                       limits = rar, oob = squish)+
+  geom_sf(data = aus_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.2)+
+  geom_sf(data = state_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.1)+
+  theme(legend.position = "none",
+        text = element_text(size = 3.5),
+        plot.title = element_text(margin = margin(0,0,2,0)),
+        plot.margin = unit(c(1,1,1,1), "mm"))
+
+# Base map with legend
+(base_legend <- base +
+    labs(fill = "Proportion")+
+    guides(fill = guide_colourbar(barwidth = 9, 
+                                  title.position = "top",
+                                  title.hjust = 0.5))+
+    theme(legend.position = "bottom"))
+llegend <- ggpubr::get_legend(base_legend)
+
+# Base map with boxes
+base_boxes <- base
+for(i in 1:8){
+  base_boxes <- base_boxes + 
+    addBoxLabel(i, color = "black", size = 0.2)
+}
+
+# Create list of insets
+inset_list <- list()
+for(i in 1:8){
+  inset_list[[i]] <- base +
+    xlim(lims$xmin[i], lims$xmax[i]) +
+    ylim(lims$ymin[i], lims$ymax[i]) +
+    labs(title = lims$inset_labs[i])+
+    theme(panel.border = element_rect(colour = "black", size=1, fill=NA),
+          plot.title = element_text(margin = margin(0,0,2,0),
+                                    size = 3.5),
+          plot.margin = unit(c(1,1,1,1), "mm"))
+}
+inset_list <- Filter(Negate(is.null), inset_list)
+
+# create final list
+lay <- rbind(c(9,1,1,1,1,2),
+             c(5,1,1,1,1,3),
+             c(6,1,1,1,1,8),
+             c(4,10,10,10,10,7))
+pha_full_inset_plt <- arrangeGrob(grobs = c(list(base_boxes), inset_list, list(llegend)), 
+                                  layout_matrix  = lay,
+                                  top = textGrob("Obese (SHAA)",gp=gpar(fontsize=6)))
+
+
+## SA2 ##
+
+# squish the top and lower 2.5 quantiles
+rar<- unname(quantile(modelled_est$summ$sa2$mu_median, p = c(0.01,0.99)))
+
+# base map
+base <- modelled_est$summ$sa2_map %>% 
+  ggplot()+
+  theme_void()+
+  geom_sf(aes(fill = mu_median), col = NA)+
+  scale_fill_viridis_c(begin = 0, end = 1, 
+                       direction = -1,
+                       option = "F", 
+                       limits = rar, oob = squish)+
+  geom_sf(data = aus_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.2)+
+  geom_sf(data = state_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.1)+
+  theme(legend.position = "none",
+        text = element_text(size = 3.5),
+        plot.title = element_text(margin = margin(0,0,2,0)),
+        plot.margin = unit(c(1,1,1,1), "mm"))
+
+# Base map with legend
+(base_legend <- base +
+    labs(fill = "Proportion")+
+    guides(fill = guide_colourbar(barwidth = 9, 
+                                  title.position = "top",
+                                  title.hjust = 0.5))+
+    theme(legend.position = "bottom"))
+llegend <- ggpubr::get_legend(base_legend)
+
+# Base map with boxes
+base_boxes <- base
+for(i in 1:8){
+  base_boxes <- base_boxes + 
+    addBoxLabel(i, color = "black", size = 0.2)
+}
+
+# Create list of insets
+inset_list <- list()
+for(i in 1:8){
+  inset_list[[i]] <- base +
+    xlim(lims$xmin[i], lims$xmax[i]) +
+    ylim(lims$ymin[i], lims$ymax[i]) +
+    labs(title = lims$inset_labs[i])+
+    theme(panel.border = element_rect(colour = "black", size=1, fill=NA),
+          plot.title = element_text(margin = margin(0,0,2,0),
+                                    size = 3.5),
+          plot.margin = unit(c(1,1,1,1), "mm"))
+}
+inset_list <- Filter(Negate(is.null), inset_list)
+
+# create final list
+lay <- rbind(c(9,1,1,1,1,2),
+             c(5,1,1,1,1,3),
+             c(6,1,1,1,1,8),
+             c(4,10,10,10,10,7))
+sa2_full_inset_plt <- arrangeGrob(grobs = c(list(base_boxes), inset_list, list(llegend)), 
+                                  layout_matrix  = lay,
+                                  top = textGrob(rf_full,gp=gpar(fontsize=6)))
+
+## JOIN and save ##
+sa2pha_joined <- arrangeGrob(grobs = list(sa2_full_inset_plt,
+                                          pha_full_inset_plt), 
+                             layout_matrix  = matrix(c(1,2), nrow = 2))
+
+jsave(filename = "phasa2mu_obesity.png", 
+      base_folder = paste0(base_folder, "/maps_lowres"),
+      plot = sa2pha_joined, square = F,
+      ratio = c(9,6),
+      square_size = 1200,
+      dpi = 300)
+
+# cleanup
+rm(base, base_boxes, llegend, base_legend, lay, sa2_full_inset_plt, pha_full_inset_plt, sa2pha_joined)
+message("---- Finished sa2pha joined")
+
 ## Sydney Habour Cut Out - alcohol ## ------------------------------------------
 
 SyndeyCutOut <- data.frame(
@@ -1140,6 +1296,162 @@ jsave(filename = paste0("lisa_", rf ,".png"),
 # cleanup
 rm(base, base_boxes, llegend, base_legend, lay, full_inset_plt)
 message("---- Finished LISA")
+
+## OR and OREP joined ## -------------------------------------------------------
+
+## OR ##
+
+# SETUP - use 2 instead!
+cut_offs <- c(1/2, 2)
+mapping_data <- modelled_est$summ$sa2_map %>% 
+  mutate() %>%
+  mutate(or_median = ifelse(or_median > cut_offs[2], cut_offs[2], or_median),
+         or_median = ifelse(or_median < cut_offs[1], cut_offs[1], or_median))
+
+# define fill colours
+Fill.colours <- c("#2C7BB6", "#2C7BB6", "#ABD9E9", "#FFFFBF", "#FDAE61", "#D7191C", "#D7191C")
+End <- log2(2.1)
+Breaks.fill <- c(1/2, 1/1.5, 1, 1.5, 2)
+Fill.values <- c(-End, log2(Breaks.fill), End)
+
+# base map
+base <- mapping_data %>% 
+  ggplot(aes(fill = log2(or_median)))+
+  theme_void()+
+  geom_sf(col = NA)+
+  scale_fill_gradientn(colors = Fill.colours,
+                       values = rescale(Fill.values),
+                       labels = as.character(round(Breaks.fill, 3)),
+                       breaks = log2(Breaks.fill),
+                       limits = range(Fill.values))+
+  geom_sf(data = aus_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.2)+
+  geom_sf(data = state_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.1)+
+  theme(legend.position = "none",
+        text = element_text(size = 5),
+        plot.title = element_text(margin = margin(0,0,2,0)),
+        plot.margin = unit(c(1,1,1,1), "mm"))
+
+# Base map with legend
+(base_legend <- base +
+    labs(fill = "Odds ratio")+
+    guides(fill = guide_colourbar(barwidth = 9, 
+                                  title.position = "top",
+                                  title.hjust = 0.5))+
+    theme(legend.position = "bottom"))
+llegend <- ggpubr::get_legend(base_legend)
+
+# Base map with boxes
+base_boxes <- base
+for(i in 1:8){
+  base_boxes <- base_boxes + 
+    addBoxLabel(i, color = "black", size = 0.2)
+}
+
+# Create list of insets
+inset_list <- list()
+for(i in 1:8){
+  inset_list[[i]] <- base +
+    xlim(lims$xmin[i], lims$xmax[i]) +
+    ylim(lims$ymin[i], lims$ymax[i]) +
+    labs(title = lims$inset_labs[i])+
+    theme(panel.border = element_rect(colour = "black", size=1, fill=NA),
+          plot.title = element_text(margin = margin(0,0,2,0),
+                                    size = 3.5),
+          plot.margin = unit(c(1,1,1,1), "mm"))
+}
+inset_list <- Filter(Negate(is.null), inset_list)
+
+# create final list
+lay <- rbind(c(9,1,1,1,1,2),
+             c(5,1,1,1,1,3),
+             c(6,1,1,1,1,8),
+             c(4,10,10,10,10,7))
+or_full_inset_plt <- arrangeGrob(grobs = c(list(base_boxes), inset_list, list(llegend)), 
+                                 layout_matrix  = lay,
+                                 top = textGrob(rf_full,gp=gpar(fontsize=6)))
+
+## EP ##
+
+# SETUP
+mapping_data <- modelled_est$summ$sa2_map %>% 
+  mutate(or_EP = ifelse(or_EP == 0, 0.001, or_EP),
+         or_EP = ifelse(or_EP == 1, 0.999, or_EP))
+
+# base map
+base <- mapping_data %>% 
+  ggplot()+
+  theme_void()+
+  geom_sf(aes(fill = or_EP), col = NA)+
+  scale_fill_gradientn(colors = c("#008837", "#a6dba0", "white","white","white", "#c2a5cf", "#7b3294"),
+                       limits = c(-0.0000001,1.0000001),
+                       #oob = squish,
+                       #trans = "logit",
+                       breaks = c(0,0.2,0.25,0.5,0.75,0.8,1),
+                       labels = as.character(c(0,0.2,"",0.5,"",0.8,1)))+
+  geom_sf(data = aus_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.2)+
+  geom_sf(data = state_border, aes(geometry = geometry), 
+          colour = "black", fill = NA, size = 0.1)+
+  theme(legend.position = "none",
+        text = element_text(size = 5),
+        plot.title = element_text(margin = margin(0,0,2,0)),
+        plot.margin = unit(c(1,1,1,1), "mm"))
+
+# Base map with legend
+(base_legend <- base +
+    labs(fill = "Exceedance probability")+
+    guides(fill = guide_colourbar(barwidth = 9, 
+                                  title.position = "top",
+                                  title.hjust = 0.5))+
+    theme(legend.position = "bottom"))
+llegend <- ggpubr::get_legend(base_legend)
+
+# Base map with boxes
+base_boxes <- base
+for(i in 1:8){
+  base_boxes <- base_boxes + 
+    addBoxLabel(i, color = "black", size = 0.2)
+}
+
+# Create list of insets
+inset_list <- list()
+for(i in 1:8){
+  inset_list[[i]] <- base +
+    xlim(lims$xmin[i], lims$xmax[i]) +
+    ylim(lims$ymin[i], lims$ymax[i]) +
+    labs(title = lims$inset_labs[i])+
+    theme(panel.border = element_rect(colour = "black", size=1, fill=NA),
+          plot.title = element_text(margin = margin(0,0,2,0),
+                                    size = 3.5),
+          plot.margin = unit(c(1,1,1,1), "mm"))
+}
+inset_list <- Filter(Negate(is.null), inset_list)
+
+# create final list
+lay <- rbind(c(9,1,1,1,1,2),
+             c(5,1,1,1,1,3),
+             c(6,1,1,1,1,8),
+             c(4,10,10,10,10,7))
+orep_full_inset_plt <- arrangeGrob(grobs = c(list(base_boxes), inset_list, list(llegend)), 
+                                   layout_matrix  = lay)
+
+## JOIN and save ##
+or_joined <- arrangeGrob(grobs = list(or_full_inset_plt,
+                                      orep_full_inset_plt), 
+                         layout_matrix  = matrix(c(1,2), nrow = 2))
+
+jsave(filename = paste0("orjoined_", rf ,".png"), 
+      base_folder = paste0(base_folder, "/maps_lowres"),
+      plot = or_joined, square = F,
+      ratio = c(9,6),
+      square_size = 1200,
+      dpi = 300)
+
+# cleanup
+rm(base, base_boxes, llegend, base_legend, lay, or_full_inset_plt, orep_full_inset_plt, or_joined)
+message("---- Finished OR_EP joined")
 
 ## FINISH FOR LOOP #### --------------------------------------------------------
 
